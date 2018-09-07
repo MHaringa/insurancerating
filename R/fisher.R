d49de99aa56c2ffe9887a065398cb2c254da56b6
MHaringa <mtharinga@gmail.com>
2018-09-07 08:38:13 +0200

    init

---
+++ R/fisher.R
@@ -0,0 +1,27 @@
+#' Fisher's natural breaks classification
+#'
+#' @description The functions provides an interface to finding class intervals for continuous numerical variables, for example for choosing colours for plotting.
+#'
+#' @param vec
+#' @param n
+#'
+#' @return Vector with clustering
+#'
+#' @import classInt
+#'
+#' @author Martin Haringa
+#'
+#' @details The "fisher" style uses the algorithm proposed by W. D. Fisher (1958) and discussed by Slocum et al. (2005) as the Fisher-Jenks algorithm. This function is adopted from the classInt package.
+#'
+#' @references Fisher, W. D. 1958 "On grouping for maximum homogeneity", Journal of the American Statistical Association, 53, pp. 789–798 (http://lib.stat.cmu.edu/cmlib/src/cluster/fish.f)
+#'
+#'
+#' @export
+fisher <- function(vec, n = 7){
+  cluster <- classIntervals(vec, n = n, style = 'fisher', intervalClosure = 'right')[[2]]
+  vec_cut <- cut(vec, breaks = cluster, include.lowest = TRUE, dig.lab = 2)
+  return(vec_cut)
+}
+
+
+
